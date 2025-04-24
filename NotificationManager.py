58fdbab2d7165542056b99788a20cd8c726b6c70
lil David <1337lilDavid@gmail.com>
2025-04-23 22:48:22 -0500

    Fix notifications

--- NotificationManager.py
+++ NotificationManager.py
@@ -1,5 +1,5 @@
 import time
-from typing import Callable, List
+from typing import Any, Callable, Coroutine, List
 
 
 class NotificationManager:
@@ -7,10 +7,10 @@ class NotificationManager:
     time_since_last_message: float = 0
     last_message_time: float = 0
     message_duration: float
-    send_notification_func: Callable[[str], bool]
+    send_notification_func: Callable[[str], Coroutine[Any, Any, bool]]
 
     def __init__(
-        self, message_duration: float, send_notification_func: Callable[[str], bool]
+        self, message_duration: float, send_notification_func: Callable[[str], Coroutine[Any, Any, bool]]
     ):
         self.message_duration = (
             message_duration / 2
@@ -20,14 +20,14 @@ class NotificationManager:
     def queue_notification(self, message: str):
         self.notification_queue.append(message)
 
-    def handle_notifications(self):
+    async def handle_notifications(self):
         self.time_since_last_message = time.time() - self.last_message_time
         if (
             len(self.notification_queue) > 0
             and self.time_since_last_message >= self.message_duration
         ):
             notification = self.notification_queue[0]
-            result = self.send_notification_func(notification)
+            result = await self.send_notification_func(notification)
             if result:
                 self.notification_queue.pop(0)
                 self.last_message_time = time.time()
